f1a1be0dc1d0e62ce3f739f9a798d9ac9a965df3
Abdelrazek Emad <a.razek@bevatel.com>
2025-06-09 20:23:28 +0300

    Nuxt Ui and PWA installation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@vite-pwa/nuxt'
+  ],
+  css: ['~/assets/css/main.css'],
+  future: {
+    compatibilityVersion: 4
+  },
+  compatibilityDate: '2024-11-27',
+  pwa: {
+    manifest: {
+      name: 'Rezo Tracker',
+      short_name: 'Rezo',
+      lang: 'en',
+      display: 'standalone',
+      start_url: '/',
+      background_color: '#ffffff',
+      theme_color: '#00c16a',
+    },
+    meta: {
+      mobileApp: true,
+      mobileAppIOS: true,
+      appleStatusBarStyle: 'default',
+    },
+    icon: {
+      source: 'public/icon.png',
+    },
+  }
 })
